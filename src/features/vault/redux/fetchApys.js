074888e9037c96b1fdf2054f73b62b2e2a89d289
krypto-dev <kryptodev7@gmail.com>
2022-02-23 05:35:57 +1000

    add tokenlist

--- src/features/vault/redux/fetchApys.js
+++ src/features/vault/redux/fetchApys.js
@@ -16,7 +16,7 @@ export function fetchApys() {
 
     return new Promise((resolve, reject) => {
       const cacheBuster = getApiCacheBuster();
-      const doRequest = axios.get(`https://api.bomb.farm/apy/breakdown?_=${cacheBuster}`);
+      const doRequest = axios.get(`http://localhost:3000/apy/breakdown?_=${cacheBuster}`);
 
       doRequest.then(
         res => {
